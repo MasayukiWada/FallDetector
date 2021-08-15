d04fa57d105af583212e6321c19649861520058c
Masayuki Wada <wada_muramasa@nifty.com>
2021-08-15 19:23:09 +0900

    Initial Commit

---
+++ FallDetector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FallDetector
+//
+//  Created by Masayuki Wada on 2021/08/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
